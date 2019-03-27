dda9890f0e1129fb4adbbe5dbd33e1b2f2d81908
tulf <timzulf@gmail.com>
2019-03-27 15:10:34 -0400

    authentication through authlock

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,18 @@
 import React, { useEffect } from "react";
 import auth from "../Auth.js";
+import { withRouter } from "react-router-dom";
 
 const Login = props => {
 	useEffect(() => {
-		auth.login();
+		if (localStorage.getItem('token')){
+			props.history.push('/schedule');
+		}
+		else{
+			auth.show();
+		}
+
 	});
 	return <p>Redirecting you to login page...</p>;
 };
 
-export default Login;
+export default withRouter(Login);

108b026443e7435b2ce485ad0f2122477d90ef50
Alvin <alvin.frierson@fuller.edu>
2019-03-28 12:48:32 -0700

    merging

--- src/components/views/WorkoutsView/index.js
+++ src/components/views/WorkoutsView/index.js
@@ -2,36 +2,36 @@ import React from "react";
 import Protected from "../../Protected";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
-import Workout from './WorkoutComponents/Workout.js';
 
 const getWorkouts = gql`
-{
-  getSavedWorkouts{
-      id
-      name
-      exercises {
-        id
-        name
-        sets
-        intervals
-        intensity
-        duration
-      }
-    }
-}
+	{
+		getWorkouts {
+			id
+			name
+			exercises {
+				id
+				name
+				interval
+				rep
+				set
+				duration
+				intensity
+			}
+		}
+	}
 `;
 
 const Workouts = props => {
-    return (
-      <Query query={getWorkouts}>
-        {({loading, error, data}) => {
-          if(loading) return <p>Loading...</p>;
-          if(error) return <p>{error.message}</p>;
-          console.log(data);
-          return <>{data.getSavedWorkouts.map(w => <Workout workout={w} />)}</>
-        }}
-      </Query>
-    )
-  }
+	return (
+		<Query query={getWorkouts}>
+			{({ loading, error, data }) => {
+				if (loading) return <p>Loading...</p>;
+				if (error) return <p>{error.message}</p>;
+        console.log(data);
+        return <>{data.getSavedWorkouts.map(w => <Workout workout={w} />)}</>
+			}}
+		</Query>
+	);
+};
 
-export default (Workouts);
+export default Protected(Workouts);

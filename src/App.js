55e058b5cc433e170733b5f080c6e4ed686bc038
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-28 09:33:20 -0700

    Added userLogin query to run when a user first logs in and is redirected to Schedules view. This creates the user in our database if it is not there already. Removed unused variables from components. Added yarn.lock to gitignore and deleted it from Repo.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
 import { withRouter } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Routes from "./components/Routes";
-
-const getInfo = gql`
-	{
-		info
-	}
-`;
 
 class App extends Component {
 	render() {

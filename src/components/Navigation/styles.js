df77740b4925050e813f9a005992956fa6d42af1
MichaelNey <ney.quentin.michael@gmail.com>
2019-04-01 16:13:00 -0700

    Navigation component is now mobile responsive. Converts to Hamburger toggle menu when screen is 640px or less.

--- src/components/Navigation/styles.js
+++ src/components/Navigation/styles.js
@@ -2,8 +2,37 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const Container = styled.div`
+	position: relative;
 	background: #1a8fff;
 	padding: 4px;
+`;
+
+export const Links = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	@media (max-width: 640px) {
+		flex-direction: column;
+		display: none;
+		&.open {
+			display: flex;
+		}
+	}
+`;
+
+export const NavToggle = styled.div`
+	width: 100%;
+	text-align: right;
+	color: white;
+	font-size: 32px;
+	display: none;
+	padding-right: 32px;
+	i:hover {
+		cursor: pointer;
+	}
+	@media (max-width: 640px) {
+		display: block;
+	}
 `;
 
 export const Link = styled(NavLink)`
@@ -21,7 +50,13 @@ export const Link = styled(NavLink)`
 		width: 0%;
 		left: 0;
 		content: "";
-		transition: 0.3s all;
+		transition: 0.4s all;
+		@media (max-width: 640px) {
+			left: 33%;
+			max-width: 33%;
+			transition: 1s all;
+			text-align: center;
+		}
 	}
 	&:hover {
 		text-decoration: none;

960b66d68b4a9754e6ecc314c76e4640ff010887
tulf <timzulf@gmail.com>
2019-04-05 14:49:58 -0400

    useffect to prohibit access to landing page

--- src/components/views/Landing/index.js
+++ src/components/views/Landing/index.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 import * as s from "./styles";
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
 const Landing = props => {
+	useEffect(() => {
+		if(localStorage.getItem('token')) {
+			props.history.push('/schedule')
+		}
+	}, []);
+
 	return <s.Container>
 		<div class ="jumbotron">
 		<p>CleanLift is a workout tracking platform to help keep you motivated and on schedule. The platform allows you to schedule workout sessions, add workouts to your scheduled dates, and while you're in the gym you can even keep track of what you have completed. It doesn't end after the workout though, we allow you to record Body Metrics and keep track of your progress over time. To start your path to greater health use CleanLift."</p>
@@ -11,4 +17,8 @@ const Landing = props => {
 	</s.Container>;
 };
 
-export default Landing;
+export default withRouter(Landing);
+
+
+
+

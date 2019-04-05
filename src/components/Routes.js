ee4546a7687de40b1b62f4e9ea28612a3e9401a3
Michael Ney <ney.quentin.michael@gmail.com>
2019-04-05 02:54:49 -0700

    Update Routes.js

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,6 +1,6 @@
+    
 import React from "react";
 import { Route, withRouter } from "react-router-dom";
-import styled from "styled-components";
 import Callback from "./callback.js";
 import Landing from "./views/Landing";
 import Login from "./Login";
@@ -12,17 +12,16 @@ import Progress from "./views/Progress";
 
 const Routes = props => {
 	return (
-		<ContentContainer>
+		<>
 			<Route exact path="/" component={Landing} />
 			<Route exact path="/login" component={Login} />
 			<Route exact path="/logout" component={Logout} />
 			<Route path="/schedule" component={Schedule} />
 			<Route path="/callback" component={Callback} />
-			<Route path="/workouts" component={Workouts} />
-			<Route exact path="/payment" component={Payment} />
+			<Route exact path="/workouts" component={Workouts} />
 			<Route path="/settings" component={Settings} />
 			<Route exact path="/progress" component={Progress} />
-		</ContentContainer>
+		</>
 	);
 };
 

0a2ac68571a8f69b15dceb44ec92d8766a6aae8f
MichaelNey <ney.quentin.michael@gmail.com>
2019-04-11 09:31:56 -0700

    Added react-toastify container, and onError function to ApolloClient, so if we get a 401, then the user is forced to log in again because their JWT expired.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ import App from "./App";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./StyleTheme.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.min.css";
+
+const logout = () => {
+	localStorage.removeItem("token");
+	window.location = "/login";
+};
 
 const client = new ApolloClient({
 	uri: process.env.REACT_APP_GQL_API,
+	onError: ({ networkError }) => {
+		if (networkError.statusCode === 401) logout();
+	},
 	headers: {
 		authorization: localStorage.getItem("token")
 			? `Bearer ${localStorage.getItem("token")}`
@@ -22,7 +32,10 @@ ReactDOM.render(
 	<ApolloProvider client={client}>
 		<Router>
 			<ThemeProvider theme={theme}>
-				<App />
+				<>
+					<App />
+					<ToastContainer />
+				</>
 			</ThemeProvider>
 		</Router>
 	</ApolloProvider>,

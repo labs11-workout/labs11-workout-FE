df8d5c6edca5c8d6d1d334502ad6b2ce317108cf
MichaelNey <ney.quentin.michael@gmail.com>
2019-04-15 13:21:16 -0700

    Added input field text to Body Metric creation and edit forms.

--- src/components/views/Progress/BodyMetrics/AddBodyMetric.js
+++ src/components/views/Progress/BodyMetrics/AddBodyMetric.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import * as s from "../styles.js";
-import { FormGroup, Label, Input, Modal, ModalHeader } from "reactstrap";
+import {
+	FormGroup,
+	Label,
+	Input,
+	Modal,
+	ModalHeader,
+	InputGroup,
+	InputGroupText
+} from "reactstrap";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 
@@ -65,35 +73,49 @@ const AddBodyMetric = () => {
 								<Label for="exampleEmail" className="mr-sm-2">
 									Weight
 								</Label>
-								<Input
-									type="number"
-									min="0"
-									value={weight}
-									onChange={e => setWeight(Number(e.target.value))}
-								/>
+								<InputGroup>
+									<Input
+										type="number"
+										min="0"
+										name="email"
+										id="exampleEmail"
+										value={weight}
+										onChange={e => setWeight(Number(e.target.value))}
+									/>
+									<InputGroupText>kg</InputGroupText>
+								</InputGroup>
 							</FormGroup>
 							<FormGroup className="mb-2 mr-sm-2 mb-sm-0 mt-sm-4">
 								<Label for="examplePassword" className="mr-sm-2">
 									Height
 								</Label>
-								<Input
-									type="number"
-									min="0"
-									value={height}
-									onChange={e => setHeight(Number(e.target.value))}
-									placeholder={2}
-								/>
+								<InputGroup>
+									<Input
+										type="number"
+										min="0"
+										name="password"
+										id="examplePassword"
+										value={height}
+										onChange={e => setHeight(Number(e.target.value))}
+									/>
+									<InputGroupText>cm</InputGroupText>
+								</InputGroup>
 							</FormGroup>
 							<FormGroup className="mb-2 mr-sm-2 mb-sm-0 mt-sm-4">
 								<Label for="examplePassword" className="mr-sm-2">
 									Bodyfat
 								</Label>
-								<Input
-									type="number"
-									min="0"
-									value={bodyfat}
-									onChange={e => setBodyFat(Number(e.target.value))}
-								/>
+								<InputGroup>
+									<Input
+										type="number"
+										min="0"
+										name="password"
+										id="examplePassword"
+										value={bodyfat}
+										onChange={e => setBodyFat(Number(e.target.value))}
+									/>
+									<InputGroupText>%</InputGroupText>
+								</InputGroup>
 							</FormGroup>
 							<s.AddButton onClick={Toggle} type="submit">
 								Submit
--- src/components/views/Progress/BodyMetrics/EditBodyMetric.js
+++ src/components/views/Progress/BodyMetrics/EditBodyMetric.js
@@ -1,6 +1,15 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import * as s from "../styles.js";
-import { FormGroup, Label, Input, Button, Modal, ModalHeader } from "reactstrap";
+import {
+	FormGroup,
+	Label,
+	Input,
+	Modal,
+	ModalHeader,
+	InputGroup,
+	InputGroupText,
+	Button
+} from "reactstrap";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 
@@ -18,12 +27,17 @@ const getBodyMetrics = gql`
 
 const editBodyMetric = gql`
 	mutation EditBodyMetric(
-        $id: ID!
-        $height: Float
+		$id: ID!
+		$height: Float
 		$weight: Float
 		$bodyfat: Float
-    ) {
-		editBodyMetric(id:$id height:$height weight:$weight bodyfat:$bodyfat) {
+	) {
+		editBodyMetric(
+			id: $id
+			height: $height
+			weight: $weight
+			bodyfat: $bodyfat
+		) {
 			height
 			weight
 			bodyfat
@@ -31,60 +45,99 @@ const editBodyMetric = gql`
 	}
 `;
 
-const EditBodyMetric = ({metric} ) => {
-    const [toggle, setState] = useState(false); 
+const EditBodyMetric = ({ metric }) => {
+	const [toggle, setState] = useState(false);
 	const [height, setHeight] = useState(0);
 	const [weight, setWeight] = useState(0);
 	const [bodyfat, setBodyFat] = useState(0);
 
 	const Toggle = () => {
 		setState(!toggle);
-		console.log(toggle)
-    };
-    const SubmitForm = (e, editBodyMetric) => {
-        e.preventDefault();
-        editBodyMetric({ variables: {id:metric.id, height, bodyfat, weight}})
-    }
-    return(
-        <>
+		console.log(toggle);
+	};
+	const SubmitForm = (e, editBodyMetric) => {
+		e.preventDefault();
+		editBodyMetric({ variables: { id: metric.id, height, bodyfat, weight } });
+	};
+	return (
+		<>
+			<s.UpdateButton onClick={Toggle}>
+				<i className="fas fa-edit" />
+			</s.UpdateButton>
 
-            <s.UpdateButton onClick={Toggle}>
-            <i className="fas fa-edit"></i>
-            </s.UpdateButton>
-
-            <Mutation
-                mutation={editBodyMetric}
-                refetchQueries={() => [{ query: getBodyMetrics }]}
-            >
-                {(editBodyMetric) => (
-                <Modal isOpen={toggle} centered size='lg'>
-                <ModalHeader>
-                    Edit Metrics
-                    <s.DeleteButton onClick={Toggle}><i className="fas fa-times"></i>
-                    </s.DeleteButton>
-                </ModalHeader>
-                    <s.CreationForm onSubmit={e => SubmitForm(e, editBodyMetric)} centered>
-                        <FormGroup className="mb-2 mr-sm-2 mb-sm-0 mt-sm-4">
-                            <Label for="exampleEmail" className="mr-sm-2">Weight</Label>
-                            <Input type="number" min="0" name="email" id="exampleEmail" value={weight} onChange={e => setWeight(Number(e.target.value))}/>
-                        </FormGroup>
-                        <FormGroup className="mb-2 mr-sm-2 mb-sm-0 mt-sm-4">
-                            <Label for="examplePassword" className="mr-sm-2">Height</Label>
-                            <Input type="number" min="0" name="password" id="examplePassword" value={height} onChange={e => setHeight(Number(e.target.value))}/>
-                        </FormGroup>
-                        <FormGroup className="mb-2 mr-sm-2 mb-sm-0 mt-sm-4">
-                            <Label for="examplePassword" className="mr-sm-2">Bodyfat</Label>
-                            <Input type="number" min="0" name="password" id="examplePassword" value={bodyfat} onChange={e => setBodyFat(Number(e.target.value))}/>
-                        </FormGroup>
-                        <Button onClick={Toggle} type="submit">Submit</Button>
-
-
-                    </s.CreationForm>
-                </Modal>
-                )}
-            </Mutation>
-        </>
-    )
-}
+			<Mutation
+				mutation={editBodyMetric}
+				refetchQueries={() => [{ query: getBodyMetrics }]}
+			>
+				{editBodyMetric => (
+					<Modal isOpen={toggle} centered size="lg">
+						<ModalHeader>
+							Edit Metrics
+							<s.DeleteButton onClick={Toggle}>
+								<i className="fas fa-times" />
+							</s.DeleteButton>
+						</ModalHeader>
+						<s.CreationForm
+							onSubmit={e => SubmitForm(e, editBodyMetric)}
+							centered
+						>
+							<FormGroup className="mb-2 mr-sm-2 mb-sm-0 mt-sm-4">
+								<Label for="exampleEmail" className="mr-sm-2">
+									Weight
+								</Label>
+								<InputGroup>
+									<Input
+										type="number"
+										min="0"
+										name="email"
+										id="exampleEmail"
+										value={weight}
+										onChange={e => setWeight(Number(e.target.value))}
+									/>
+									<InputGroupText>kg</InputGroupText>
+								</InputGroup>
+							</FormGroup>
+							<FormGroup className="mb-2 mr-sm-2 mb-sm-0 mt-sm-4">
+								<Label for="examplePassword" className="mr-sm-2">
+									Height
+								</Label>
+								<InputGroup>
+									<Input
+										type="number"
+										min="0"
+										name="password"
+										id="examplePassword"
+										value={height}
+										onChange={e => setHeight(Number(e.target.value))}
+									/>
+									<InputGroupText>cm</InputGroupText>
+								</InputGroup>
+							</FormGroup>
+							<FormGroup className="mb-2 mr-sm-2 mb-sm-0 mt-sm-4">
+								<Label for="examplePassword" className="mr-sm-2">
+									Bodyfat
+								</Label>
+								<InputGroup>
+									<Input
+										type="number"
+										min="0"
+										name="password"
+										id="examplePassword"
+										value={bodyfat}
+										onChange={e => setBodyFat(Number(e.target.value))}
+									/>
+									<InputGroupText>%</InputGroupText>
+								</InputGroup>
+							</FormGroup>
+							<Button onClick={Toggle} type="submit">
+								Submit
+							</Button>
+						</s.CreationForm>
+					</Modal>
+				)}
+			</Mutation>
+		</>
+	);
+};
 
 export default EditBodyMetric;

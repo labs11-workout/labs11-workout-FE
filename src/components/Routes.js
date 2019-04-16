d92996d6fbc6feb5510d8b0fb8e43349e70225fc
MichaelNey <ney.quentin.michael@gmail.com>
2019-04-16 13:18:58 -0700

    Added landing page template and netlify redirects file update.

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -9,20 +9,42 @@ import Settings from "./views/Payment/settings";
 import Workouts from "./views/Workouts";
 import Progress from "./views/Progress";
 import Notes from "./views/Notes";
+import Navigation from "./Navigation";
+import Footer from "./Footer";
 
 const Routes = props => {
 	return (
-		<div style={{ paddingBottom: "62px" }}>
-			<Route exact path="/" component={Landing} />
-			<Route exact path="/login" component={Login} />
-			<Route exact path="/logout" component={Logout} />
-			<Route path="/schedule" component={Schedule} />
-			<Route path="/callback" component={Callback} />
-			<Route path="/workouts" component={Workouts} />
-			<Route path="/settings" component={Settings} />
-			<Route exact path="/progress" component={Progress} />
-			<Route path="/notes" component={Notes} />
-		</div>
+		<>
+			{props.location.pathname === "/" ? (
+				<div style={{ paddingBottom: "62px" }}>
+					<Route exact path="/" component={Landing} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/logout" component={Logout} />
+					<Route path="/schedule" component={Schedule} />
+					<Route path="/callback" component={Callback} />
+					<Route path="/workouts" component={Workouts} />
+					<Route path="/settings" component={Settings} />
+					<Route exact path="/progress" component={Progress} />
+					<Route path="/notes" component={Notes} />
+				</div>
+			) : (
+				<>
+					<Navigation />
+					<div style={{ paddingBottom: "62px" }}>
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/logout" component={Logout} />
+						<Route path="/schedule" component={Schedule} />
+						<Route path="/callback" component={Callback} />
+						<Route path="/workouts" component={Workouts} />
+						<Route path="/settings" component={Settings} />
+						<Route exact path="/progress" component={Progress} />
+						<Route path="/notes" component={Notes} />
+					</div>
+					<Footer />
+				</>
+			)}
+		</>
 	);
 };
 
